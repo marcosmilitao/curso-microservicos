da4582bc9bd08cce1d958c2c68b3132943eccbe3
marcosmilitao <marcosxr7@gmail.com>
2021-09-16 21:37:20 -0300

    Initial commit

---
+++ hr-payroll/src/main/java/com/militao/hrpayroll/services/PaymentService.java
@@ -0,0 +1,20 @@
+package com.militao.hrpayroll.services;
+
+import com.militao.hrpayroll.entities.Payment;
+import com.militao.hrpayroll.entities.Worker;
+import com.militao.hrpayroll.feignclients.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+
+    public Payment getPayment(long workerId, int days){
+
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+        return new Payment(worker.getName(),worker.getDailyIncome(),days);
+    }
+}
---
+++ hr-worker/src/main/java/com/militao/hrworker/resources/WorkerResource.java
@@ -0,0 +1,57 @@
+package com.militao.hrworker.resources;
+
+import com.militao.hrworker.entities.Worker;
+import com.militao.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @Value("${test.config}")
+    private String testConfig;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> configs(){
+
+        logger.info("CONFIGS"+ testConfig);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll(){
+        List<Worker> list = workerRepository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
+        Worker worker = workerRepository.findById(id).get();
+        return ResponseEntity.ok(worker);
+    }
+}
